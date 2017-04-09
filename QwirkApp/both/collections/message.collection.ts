e93d64d42f8208b93ffab83b915230d49854092e
Dramelac <203697@supinfo.com>
2017-04-09 14:41:05 +0200

    Remove insecure options

--- QwirkApp/both/collections/message.collection.ts
+++ QwirkApp/both/collections/message.collection.ts
@@ -1,15 +1,4 @@
-import { MongoObservable } from "meteor-rxjs";
-
+import {MongoObservable} from "meteor-rxjs";
 import {Message} from "../models/message.model";
 
 export const Messages = new MongoObservable.Collection<Message>("messages");
-
-function loggedIn(){
-    return !!Meteor.user();
-}
-
-Messages.allow({
-    insert: loggedIn,
-    update: loggedIn,
-    remove: loggedIn
-});
