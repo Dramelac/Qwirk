20f340ee0fde75a4eabde24b24c34c8619cc317c
Dramelac <203697@supinfo.com>
2017-04-09 20:49:08 +0200

    Add picture to profile

--- QwirkApp/client/imports/app/userSetUp/profile.component.ts
+++ QwirkApp/client/imports/app/userSetUp/profile.component.ts
@@ -1,34 +1,50 @@
-import {Component, OnInit, NgZone} from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-
-import template from './profile.component.html';
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import template from "./profile.component.html";
+import {Profile} from "../../../../both/models/profile.model";
+import {Profiles} from "../../../../both/collections/profile.collection";
+import {MeteorObservable} from "meteor-rxjs";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'profile',
     template
 })
 
-export class ProfileComponent implements OnInit{
+export class ProfileComponent implements OnInit {
     profileForm: FormGroup;
     error: string;
     currentUser = Meteor.user();
+    profile: Profile;
+    profileSub: Subscription;
 
-    constructor(private router: Router, private zone: NgZone, private formBuilder: FormBuilder) {}
+    constructor(private formBuilder: FormBuilder) {
+    }
 
 
     ngOnInit() {
+
+        if (this.profileSub){
+            this.profileSub.unsubscribe();
+        }
+
+        MeteorObservable.subscribe('profiles', this.currentUser._id).subscribe();
+        //TODO fix this
+        this.profile = Profiles.findOne({userId:this.currentUser._id});
+
         this.profileForm = this.formBuilder.group({
-            username: [this.currentUser.username,Validators.required],
-            email: [this.currentUser.emails[0].address,Validators.required],
-            newPassword:['',Validators.required],
-            confirmPassword:['',Validators.required]
+            username: [this.profile.username, Validators.required],
+            picture: [this.profile.picture, Validators.required],
+            email: [this.currentUser.emails[0].address, Validators.required],
+            newPassword: ['', Validators.required],
+            confirmPassword: ['', Validators.required]
         });
+
 
         this.error = '';
     }
 
-    profile(){
+    save() {
 
     }
 
