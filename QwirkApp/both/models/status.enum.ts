8885669d7a6c99a5132a6e06a6812c18c73ae795
Dramelac <203697@supinfo.com>
2017-04-14 20:12:05 +0200

    Add user status to nav bar

--- QwirkApp/both/models/status.enum.ts
+++ QwirkApp/both/models/status.enum.ts
@@ -5,3 +5,18 @@ export const Status =  {
     Away: 2,
     Busy: 3
 };
+
+export function StatusToString(status: number){
+    switch (status){
+        case Status.Offline:
+            return "Offline";
+        case Status.Online:
+            return "Online";
+        case Status.Away:
+            return "Away";
+        case Status.Busy:
+            return "Busy";
+        default:
+            return "Unknown";
+    }
+}
--- QwirkApp/client/imports/app/user/user-status.component.ts
+++ QwirkApp/client/imports/app/user/user-status.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from "@angular/core";
 import template from "./user-status.component.html";
 import {InjectUser} from "angular2-meteor-accounts-ui";
 import {Profile} from "../../../../both/models/profile.model";
+import {StatusToString} from "../../../../both/models/status.enum";
+import {Profiles} from "../../../../both/collections/profile.collection";
+import {MeteorObservable} from "meteor-rxjs";
 
 @Component({
     selector: "user-status",
@@ -10,15 +13,21 @@ import {Profile} from "../../../../both/models/profile.model";
 @InjectUser('user')
 export class UserStatusComponent implements OnInit {
     profile: Profile;
+    status: string;
 
     constructor() {
     }
 
     ngOnInit(): void {
-        /*MeteorObservable.subscribe('profile').subscribe(() => {
-            MeteorObservable.autorun().subscribe(() => {
-                this.profile = Profiles.findOne({userId: Meteor.userId()});
-            });
-        });*/
+        MeteorObservable.subscribe('profile').subscribe(() => {
+            this.profile = Profiles.findOne({userId: Meteor.userId()});
+            this.status = StatusToString(this.profile.status);
+            //TODO add dropdows status change
+        });
+
+    }
+
+    updateStatus(statusValue: number){
+        //TODO add status self update
     }
 }
