a02a7c3e520184ac6ecb028d08c8f3f5718999cf
Dramelac <203697@supinfo.com>
2017-05-01 22:32:18 +0200

    Update text transform regex + adding url lib

--- QwirkApp/client/imports/app/messages/messages-list.component.ts
+++ QwirkApp/client/imports/app/messages/messages-list.component.ts
@@ -8,6 +8,7 @@ import template from "./messages-list.component.html";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Chat} from "../../../../both/models/chat.model";
 import {Profiles} from "../../../../both/collections/profile.collection";
+import "autolinker";
 
 @Component({
     selector: 'messages-list',
@@ -21,9 +22,13 @@ export class MessagesListComponent implements OnInit, OnDestroy{
     messagesSub: Subscription;
     distantUserId: string;
 
+    autolinker;
+
     constructor(private route: ActivatedRoute, private router: Router){}
 
     ngOnInit() {
+        this.autolinker = new Autolinker();
+
         this.paramsSub = this.route.params
             .map(params => params["chatId"])
             .subscribe(chat => {
@@ -60,7 +65,7 @@ export class MessagesListComponent implements OnInit, OnDestroy{
                         ).map((messages: Message[]) => {
                             messages.forEach((message) => {
                                 message.ownership = Meteor.userId() == message.ownerId ? 'mine' : 'other';
-                                message.content = MessagesListComponent.processMessage(message.content);
+                                message.content = this.processMessage(message.content);
 
                                 return message;
                             });
@@ -75,16 +80,24 @@ export class MessagesListComponent implements OnInit, OnDestroy{
 
     }
 
-    static processMessage(msg: string): string{
-        // italic
-        msg = msg.replace(/\/([^/]*)\//g,"<i>$1</i>");
-        // bold
-        msg = msg.replace(/\*([^*]*)\*/g,"<b>$1</b>");
-        // underline
-        msg = msg.replace(/_([^_]*)_/g,"<u>$1</u>");
-        // strike
-        msg = msg.replace(/~([^~]*)~/g,"<del>$1</del>");
+    processMessage(msg: string): string{
+        console.log("before :", msg);
+        // url
+        //msg = msg.replace(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
+        //    "<a href='$1://$2'>$1://$2</a>");
+        msg = this.autolinker.link(msg);
 
+        // italic
+        msg = msg.replace(/[^/<]?\/([^/]+)\//g,"<i>$1</i>");
+        // bold
+        msg = msg.replace(/\*([^*]+)\*/g,"<b>$1</b>");
+        // underline
+        msg = msg.replace(/_([^_]+)_/g,"<u>$1</u>");
+        // strike
+        msg = msg.replace(/~([^~]+)~/g,"<del>$1</del>");
+
+
+        console.log("after :", msg);
         return msg;
     }
 
