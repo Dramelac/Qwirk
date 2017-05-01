904354f796b71ffd71e9363e44e2011285b762e8
Dramelac <203697@supinfo.com>
2017-05-01 22:59:42 +0200

    Fix Autolinker import syntax + update italic to backslash

--- QwirkApp/client/imports/app/messages/messages-list.component.ts
+++ QwirkApp/client/imports/app/messages/messages-list.component.ts
@@ -8,7 +8,7 @@ import template from "./messages-list.component.html";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Chat} from "../../../../both/models/chat.model";
 import {Profiles} from "../../../../both/collections/profile.collection";
-import "autolinker";
+import * as Autolinker from "autolinker";
 
 @Component({
     selector: 'messages-list',
@@ -22,13 +22,9 @@ export class MessagesListComponent implements OnInit, OnDestroy{
     messagesSub: Subscription;
     distantUserId: string;
 
-    autolinker;
-
     constructor(private route: ActivatedRoute, private router: Router){}
 
     ngOnInit() {
-        this.autolinker = new Autolinker();
-
         this.paramsSub = this.route.params
             .map(params => params["chatId"])
             .subscribe(chat => {
@@ -81,14 +77,10 @@ export class MessagesListComponent implements OnInit, OnDestroy{
     }
 
     processMessage(msg: string): string{
-        console.log("before :", msg);
-        // url
-        //msg = msg.replace(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g,
-        //    "<a href='$1://$2'>$1://$2</a>");
-        msg = this.autolinker.link(msg);
+        //console.log("before :", msg);
 
         // italic
-        msg = msg.replace(/[^/<]?\/([^/]+)\//g,"<i>$1</i>");
+        msg = msg.replace(/\\([^\\]+)\\/g,"<i>$1</i>");
         // bold
         msg = msg.replace(/\*([^*]+)\*/g,"<b>$1</b>");
         // underline
@@ -96,8 +88,10 @@ export class MessagesListComponent implements OnInit, OnDestroy{
         // strike
         msg = msg.replace(/~([^~]+)~/g,"<del>$1</del>");
 
+        // url
+        msg = Autolinker.link(msg);
 
-        console.log("after :", msg);
+        //console.log("after :", msg);
         return msg;
     }
 
