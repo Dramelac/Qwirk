2133c4c04650353444a9b4a2b5883403b5b99bab
Droopy <romain.garcia38230@hotmail.com>
2017-05-24 10:12:20 +0200

    Block contact ok

--- QwirkApp/server/methods.ts
+++ QwirkApp/server/methods.ts
@@ -194,14 +194,16 @@ Meteor.methods({
             friendId: initiator,
             chatId: chatId,
             profileId: Profiles.findOne({userId : initiator})._id,
-            displayName: Meteor.users.findOne({_id : initiator}).username
+            displayName: Meteor.users.findOne({_id : initiator}).username,
+            isBloqued : false
         };
         const contactInitiator:Contact = {
             ownerId: initiator,
             friendId: Meteor.userId(),
             chatId: chatId,
             profileId: Profiles.findOne({userId : Meteor.userId()})._id,
-            displayName: Meteor.user().username
+            displayName: Meteor.user().username,
+            isBloqued : false
         };
         Contacts.collection.insert(contactUser);
         Contacts.collection.insert(contactInitiator);
