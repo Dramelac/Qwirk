97c30f7ac07f0b43722178e5b3899dcd541b9b27
Dramelac <203697@supinfo.com>
2017-05-28 23:06:38 +0200

    Update CallNotif to group or contact name

--- QwirkApp/client/imports/app/menu/call-notif.component.ts
+++ QwirkApp/client/imports/app/menu/call-notif.component.ts
@@ -3,11 +3,9 @@ import {Observable} from "rxjs/Observable";
 import {Subscription} from "rxjs/Subscription";
 import {MeteorObservable} from "meteor-rxjs";
 import {Router} from "@angular/router";
-import {CallRequest} from "../../../../both/models/call-request.model";
-import {CallRequests} from "../../../../both/collections/call-request.collection";
-import {Profiles} from "../../../../both/collections/profile.collection";
+import {CallRequest, Chat, ChatType, Contact, Profile} from "../../../../both/models";
+import {CallRequests, Chats, Contacts, Profiles} from "../../../../both/collections";
 import template from "./call-notif.component.html";
-import {Profile} from "../../../../both/models/profile.model";
 
 @Component({
     selector: 'call-notif',
@@ -24,13 +22,21 @@ export class CallNotifComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.callRequestSub = MeteorObservable.subscribe('callrequest').subscribe(() => {
             MeteorObservable.autorun().subscribe(() => {
-                this.callRequest = CallRequests.find({targetUsersId:Meteor.userId()}).map((notifs: CallRequest[]) => {
-                    notifs.forEach((notif) => {
-                        //TODO update username to contact name
-                        //TODO update to group name
-                        let profile: Profile = Profiles.findOne({userId:notif.ownerUserId});
-                        if (profile){
-                            notif.ownerName = profile.username;
+                this.callRequest = CallRequests.find({targetUsersId: Meteor.userId()}).map((notifs: CallRequest[]) => {
+                    notifs.map((notif) => {
+                        let chat: Chat = Chats.findOne({_id: notif.chatId});
+                        if (chat) {
+                            if (chat.type === ChatType.GROUP) {
+                                notif.ownerName = chat.title;
+                            } else {
+                                let contact: Contact = Contacts.findOne({friendId: notif.ownerUserId});
+                                if (contact) {
+                                    notif.ownerName = contact.displayName;
+                                } else {
+                                    let profile: Profile = Profiles.findOne({userId: notif.ownerUserId});
+                                    notif.ownerName = profile ? profile.username : "Unknown";
+                                }
+                            }
                         }
                         return notif;
                     });
@@ -44,21 +50,21 @@ export class CallNotifComponent implements OnInit, OnDestroy {
         this.callRequestSub.unsubscribe();
     }
 
-    acceptCall(request: CallRequest){
+    acceptCall(request: CallRequest) {
         Session.set("activeCall", request.chatId);
         Session.set("callId", request._id);
         Session.set("callVideo", request.video);
         CallRequests.update(request._id, {
-            $pull:{targetUsersId:Meteor.userId()},
-            $push:{onlineUsers:Meteor.userId()}
+            $pull: {targetUsersId: Meteor.userId()},
+            $push: {onlineUsers: Meteor.userId()}
         });
-        this.router.navigate(['/chat/'+request.chatId]);
+        this.router.navigate(['/chat/' + request.chatId]);
     }
 
-    refuseCall(request: CallRequest){
+    refuseCall(request: CallRequest) {
         CallRequests.update(request._id, {
-            $pull:{targetUsersId:Meteor.userId()},
-            $push:{rejectUsers:Meteor.userId()}
+            $pull: {targetUsersId: Meteor.userId()},
+            $push: {rejectUsers: Meteor.userId()}
         });
     }
 }
