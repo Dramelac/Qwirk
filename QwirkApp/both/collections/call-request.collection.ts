09e00eb3e42352b408b6842bdf99f3e767f0389c
Dramelac <203697@supinfo.com>
2017-05-28 22:51:56 +0200

    Add member security on chat call

--- QwirkApp/both/collections/call-request.collection.ts
+++ QwirkApp/both/collections/call-request.collection.ts
@@ -1,13 +1,25 @@
 import {MongoObservable} from "meteor-rxjs";
 import {CallRequest} from "../models/call-request.model";
 import * as _ from "underscore";
+import {Chats} from "./chat.collection";
 
 export const CallRequests = new MongoObservable.Collection<CallRequest>("callrequests");
 
 CallRequests.allow({
     insert: function (userId, doc) {
-        //TODO check contact list doc.targetUserId
-        return doc.ownerUserId === userId;
+        if (doc.rejectUsers.length !== 0 ||
+            doc.onlineUsers.length > 1 ||
+            doc.onlineUsers[0] !== userId){
+            return false;
+        }
+        let chat = Chats.findOne({_id:doc.chatId,user:userId});
+        if (chat){
+            doc.targetUsersId.forEach((u)=>{
+                if (!_.contains(chat.user,u)) return false;
+            });
+            return doc.ownerUserId === userId;
+        }
+        return false;
     },
     update: function (userId, doc) {
         //TODO check field update
