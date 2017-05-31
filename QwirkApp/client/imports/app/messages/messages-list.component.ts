5ccd9aebbf3cb11fd0b34de122643d494fb150a1
Dramelac <203697@supinfo.com>
2017-05-31 13:44:32 +0200

    Navigator push notif (ts type not exist)

--- QwirkApp/client/imports/app/messages/messages-list.component.ts
+++ QwirkApp/client/imports/app/messages/messages-list.component.ts
@@ -122,6 +122,7 @@ export class MessagesListComponent implements OnInit, OnDestroy {
                         }
                         if (!_.contains(message.readBy, Meteor.userId())) {
                             message.isNew = true;
+                            MessagesListComponent.notifMesage(message);
                             //this.checkAndUpdateReadStatus(message);
                         }
                         return message;
@@ -148,8 +149,35 @@ export class MessagesListComponent implements OnInit, OnDestroy {
         });
     }
 
-    notifMesage() {
+    static notifMesage(message: Message) {  // Voyons si le navigateur supporte les notifications
+        if (!("Notification" in window)) {
+            alert("Ce navigateur ne supporte pas les notifications desktop");
+        }
 
+        // Voyons si l'utilisateur est OK pour recevoir des notifications
+        else if (Notification.permission === "granted") {
+            // Si c'est ok, créons une notification
+            var notification = new Notification("You receive a new message");
+        }
+
+        // Sinon, nous avons besoin de la permission de l'utilisateur
+        // Note : Chrome n'implémente pas la propriété statique permission
+        // Donc, nous devons vérifier s'il n'y a pas 'denied' à la place de 'default'
+        else if (Notification.permission !== 'denied') {
+            Notification.requestPermission(function (permission) {
+
+                // Quelque soit la réponse de l'utilisateur, nous nous assurons de stocker cette information
+                if (!('permission' in Notification)) {
+                    Notification.permission = permission;
+                }
+
+                // Si l'utilisateur est OK, on crée une notification
+                if (permission === "granted") {
+                    var notification = new Notification("You receive a new message");
+                }
+
+            });
+        }
     }
 
     checkAndUpdateReadStatus(msg: Message) {
