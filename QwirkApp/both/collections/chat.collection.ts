970b1cf16b7e6ed472422710e846c0adfba18a94
Droopy\Romain <romain.garcia38230@hotmail.com>
2017-06-05 17:47:31 +0200

    securite ok pour les chats et friendsrequest

--- QwirkApp/both/collections/chat.collection.ts
+++ QwirkApp/both/collections/chat.collection.ts
@@ -1,37 +1,49 @@
 import {MongoObservable} from "meteor-rxjs";
 import {Chat, ChatType} from "../models/chat.model";
-import _ = require("underscore");
+import * as _ from "underscore";
 
 export const Chats = new MongoObservable.Collection<Chat>("chats");
 
-function loggedIn(){
-    return !!Meteor.user();
+function checkUser(modifier) : boolean{
+    if(modifier.$push){
+        let schema = {$push : { user : {$each: modifier.$push.user.$each}}};
+        if(modifier == schema.toString){
+            return true;
+        }
+    }
+    return false;
 }
 
-//TODO add security to chat operation
 Chats.allow({
     //On vérifie que coté client on ne peut inserer que des groupes
     insert: function (userId, doc) {
-        if(doc.type === ChatType.CHAT || doc.user.length < 2 ||
-        doc.title === "" || doc.title === null){
-            return false;
-        } else {
-            return true;
-        }
+
+        return doc.type === ChatType.GROUP && doc.user.length >= 2 &&
+            doc.title !== "" && doc.title !== null
     },
     update: function (userId, doc, fields, modifier) {
         if(doc.publicly){
             if(fields.length === 1 && _.contains(fields,"user")){
-                let schema = {$push : { user : {$each: modifier.$push.user.$each}}};
-                if(modifier == schema.toString){
-                    return true;
-                }
+                return checkUser(modifier);
             }
         }
         if(_.contains(doc.admin, userId)){
-            return true;
+            if(fields.length === 1 && _.contains(fields,"user")){
+                return checkUser(modifier);
+            }
+            if(fields.length === 1 && _.contains(fields,"title")){
+                return true;
+            }
+            if(fields.length === 1 && _.contains(fields,"publicly")){
+                return true;
+            }
+            if(fields.length === 1 && _.contains(fields,"picture")){
+                return true;
+            }
         }
         return false;
     },
-    remove: loggedIn
+    remove: function (userId, doc) {
+        return userId === doc.ownerId;
+    }
 });
--- QwirkApp/both/collections/friend-request.collection.ts
+++ QwirkApp/both/collections/friend-request.collection.ts
@@ -1,13 +1,3 @@
 import {FriendRequest} from "../models/friend-request.model";
 import {MongoObservable} from "meteor-rxjs";
 export const FriendsRequest = new MongoObservable.Collection<FriendRequest>("friendRequest");
-
-function loggedIn(){
-    return !!Meteor.user();
-}
-
-FriendsRequest.allow({
-    insert: loggedIn,
-    update: loggedIn,
-    remove: loggedIn
-});
