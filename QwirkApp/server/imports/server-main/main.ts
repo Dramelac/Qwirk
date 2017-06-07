412cccee05bf42aa50ac0b03cec3f4072c233586
Dramelac <203697@supinfo.com>
2017-06-07 16:10:35 +0200

    Update mail template

--- QwirkApp/server/imports/server-main/main.ts
+++ QwirkApp/server/imports/server-main/main.ts
@@ -8,10 +8,18 @@ export class Main {
         Accounts.emailTemplates.siteName = "Qwirk";
         Accounts.emailTemplates.from     = "Qwirk <noreply@qwirk.eu>";
 
-        Accounts.emailTemplates.resetPassword.text = function (user, url) {
-            url = url.replace('#/', '');
-            return "Hello ,\n\nTo reset your password, simply click the link below.\n\n" + url +
-                "\n\nThanks.";
+        Accounts.emailTemplates.resetPassword = {
+            subject() {
+                return "[Qwirk] Reset your password";
+            },
+            text( user, url ) {
+                let urlWithoutHash = url.replace( '#/', '' ),
+                    supportEmail   = "support@qwirk.eu",
+                    emailBody      = `Hello ,\n\nTo reset your password, follow the link below.\n\n${urlWithoutHash}\n\nIf you are not initiator of this action, you can contact us at this address: ${supportEmail}\n\nBest regards,\nQwirk Team`;
+
+                return emailBody;
+            }
+
         };
 
         Accounts.emailTemplates.verifyEmail = {
@@ -22,7 +30,7 @@ export class Main {
                 let emailAddress   = user.emails[0].address,
                     urlWithoutHash = url.replace( '#/', '' ),
                     supportEmail   = "support@qwirk.eu",
-                    emailBody      = `To verify your email address (${emailAddress}) visit the following link:\n\n${urlWithoutHash}\n\n If you did not request this verification, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
+                    emailBody      = `To verify your email address (${emailAddress}) visit the following link:\n\n${urlWithoutHash}\n\n If you did not request this verification, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.\n\nBest regards,\nQwirk Team`;
 
                 return emailBody;
             }
