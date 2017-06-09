2c77e9a45532111b0742d40797e54a8b99f9a367
Dramelac <203697@supinfo.com>
2017-06-09 12:32:23 +0200

    Add success to mail verify page

--- QwirkApp/client/imports/app/auth/verify-mail.component.ts
+++ QwirkApp/client/imports/app/auth/verify-mail.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnDestroy, OnInit} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 import {Subscription} from "rxjs/Subscription";
-
 import template from "./verify-mail.component.html";
 
 @Component({
@@ -11,19 +10,24 @@ export class VerifyMailComponent implements OnInit, OnDestroy {
     token: string;
     paramsSub: Subscription;
 
-    error :string;
+    error: string;
+    success: boolean;
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(private route: ActivatedRoute) {
+    }
 
     ngOnInit() {
         this.error = "";
+        this.success = false;
         this.paramsSub = this.route.params
             .map(params => params["token"])
             .subscribe(token => {
                 this.token = token;
                 Accounts.verifyEmail(this.token, (err) => {
-                    if (err){
+                    if (err) {
                         this.error = err.reason;
+                    } else {
+                        this.success = true;
                     }
                 })
             });
